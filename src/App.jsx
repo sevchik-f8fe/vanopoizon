5f2db86a535a048e1875daba3c3dcff07950eec3
sevchik-f8fe <kononovseva0627@gmail.com>
2024-10-07 10:21:30 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ function App() {
     <>
       <div class="Main">
         <h1>Тестовое приложение</h1>
-        <img src="{{ url_for('static', filename='bot.png') }}" alt="123" />
         <p>dasfasdfasdf</p>
         <button class="btn">Кнопка</button>
       </div>
