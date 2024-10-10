84564fd1b02e4d60eadb2629d74dcbc8859995dd
sevchik-f8fe <kononovseva0627@gmail.com>
2024-10-10 13:49:49 +0300

    add points page

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
+import PointsPage from './pages/PointsPage';
 import './index.css'
 import { theme } from './utils/theme';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/vanopoizon/",
-//     element: <HomePage />,
-//     errorElement: <ErrorPage />
-//   },
-//   {
-//     path: "/vanopoizon/profile/",
-//     element: <ProfilePage />
-//   }
-// ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -32,9 +19,10 @@ createRoot(document.getElementById('root')).render(
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/points' element={<PointsPage />} />
+          {/* <Route path='/' element={<PointsPage />} /> */}
         </Routes>
       </BrowserRouter>
-      {/* <RouterProvider router={router} /> */}
     </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/pages/PointsPage.jsx
@@ -0,0 +1,173 @@
+import { Box, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import CurrencyRubleIcon from '@mui/icons-material/CurrencyRuble';
+import { backButton } from "@telegram-apps/sdk";
+
+const PointsPage = () => {
+    const navigate = useNavigate();
+    let tg = window.Telegram.WebApp;
+
+    let backBtn = tg?.BackButton;
+
+    tg.onEvent('backButtonClicked', function () {
+        navigate('/');
+        backBtn.hide();
+    });
+
+    return (
+        <Box
+            sx={{
+                p: '1em',
+                w: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '1em',
+            }}
+        >
+            <PointsContainer />
+            <DescriptionContainer />
+            <HistoryContainer />
+        </Box >
+    );
+}
+
+const PointsContainer = () => {
+    return (
+        <Box
+            sx={{
+                p: '1em',
+                borderRadius: '1em',
+                backgroundColor: '#fff',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontWeight: '700',
+                        fontSize: '4em',
+                        color: '#F34213',
+                        lineHeight: '.8'
+                    }}
+                >0</Typography>
+                <Typography
+                    sx={{
+                        fontSize: '1.3em',
+                        fontWeight: '700',
+                        color: '#F34213',
+                    }}
+                >баллов</Typography>
+            </Box>
+
+            <Box
+                sx={{
+                    backgroundColor: '#F34213',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: '50%',
+                    p: '1em',
+                }}
+            >
+                <CurrencyRubleIcon
+                    sx={{
+                        color: "#fff",
+                        fontSize: '3em'
+                    }}
+                />
+            </Box>
+        </Box>
+    );
+}
+
+const DescriptionContainer = () => {
+    let tg = window.Telegram.WebApp;
+    let backBtn = tg?.BackButton;
+
+    return (
+        <Box
+            sx={{
+                p: '1em',
+                borderRadius: '1em',
+                backgroundColor: '#2E2E3A',
+            }}
+        >
+            <Typography
+                sx={{
+                    fontWeight: '500',
+                    color: '#fff',
+                    fontSize: '.9em',
+                    mb: '1em',
+                }}
+            >
+                Дальше ты будешь получать
+                <span style={{ color: '#F34213' }}> +50 баллов</span> за каждый завершенный заказ. Подробности
+                <Link
+                    to={`/profile`}
+                    onClick={() => {
+                        if (backBtn.isVisible()) backBtn.show();
+                    }}
+                >
+                    <span style={{ color: '#5685ca' }}> в профиле</span>
+                </Link>
+            </Typography>
+            <Typography
+                sx={{
+                    fontWeight: '500',
+                    color: '#fff',
+                    fontSize: '.9em'
+                }}
+            >
+                При оформлении заказа ты можешь потратить накопленные баллы. 1 балл = 1<CurrencyRubleIcon sx={{ fontSize: '.9em' }} />.
+            </Typography>
+        </Box>
+    );
+}
+
+const HistoryContainer = () => {
+    return (
+        <Box
+            sx={{
+                p: '1em',
+                borderRadius: '1em',
+                backgroundColor: '#2E2E3A',
+            }}
+        >
+            <Typography
+                sx={{
+                    color: '#fff',
+                    fontSize: '1.2em',
+                    fontWeight: '700',
+                    mb: '.5em',
+                }}
+            >История баллов</Typography>
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '.5em',
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontSize: '.9em',
+                        fontWeight: '400',
+                        color: '#ffffff50'
+                    }}
+                >Пока что у тебя не истории начислений баллов.</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default PointsPage;
