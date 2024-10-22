829d1a4ca057a6d7980fa90073053a7d0f80e7dc
sevchik-f8fe <kononovseva0627@gmail.com>
2024-10-22 19:17:36 +0300

    fix

--- src/components/BottomBoard/BottomBoard.jsx
+++ src/components/BottomBoard/BottomBoard.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -14,95 +14,107 @@ const BottomBoard = () => {
             {isVisible ? (
                 <Box
                     sx={{
-                        p: '.2em .5em',
+                        p: '.5em',
                         position: 'fixed',
                         bottom: 0,
                         left: 0,
                         backgroundColor: '#202029',
                         minWidth: '100%',
                         zIndex: 100,
-                        display: 'flex',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                        gap: '1em',
-                        WebkitTransform: 'translateZ(0)'
+                        WebkitTransform: 'translate(0)',
+                        transform: 'translate(0)',
                     }}
                 >
-                    <IconButton
-                        onClick={() => {
-                            navigate('/');
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            alignItems: 'center',
+                            gap: '1em',
                         }}
-                        size="small"
-                        disabled={currentPage == 'home'}
-
                     >
-                        <HomeIcon
-                            sx={
-                                (currentPage == 'home') ? (
-                                    {
-                                        fontSize: '1.3em',
-                                        color: '#F34213',
-                                    }
-                                ) : (
-                                    {
-                                        fontSize: '1.3em',
-                                        color: '#fff5',
-                                    }
-                                )
-                            }
-                        />
-                    </IconButton>
+                        <IconButton
+                            onClick={() => {
+                                navigate('/');
+                            }}
+                            size="small"
+                            disabled={currentPage == 'home'}
 
-                    <IconButton
-                        disabled={currentPage == 'cart'}
-                        onClick={() => {
-                            setCurrentPage('cart');
-                            navigate('/cart');
+                        >
+                            <HomeIcon
+                                sx={
+                                    (currentPage == 'home') ? (
+                                        {
+                                            fontSize: '1.3em',
+                                            color: '#F34213',
+                                        }
+                                    ) : (
+                                        {
+                                            fontSize: '1.3em',
+                                            color: '#fff5',
+                                        }
+                                    )
+                                }
+                            />
+                        </IconButton>
+
+                        <IconButton
+                            disabled={currentPage == 'cart'}
+                            onClick={() => {
+                                setCurrentPage('cart');
+                                navigate('/cart');
+                            }}
+                            size="small"
+
+                        >
+                            <ShoppingCartIcon
+                                sx={
+                                    (currentPage == 'cart') ? (
+                                        {
+                                            fontSize: '1.3em',
+                                            color: '#F34213',
+                                        }
+                                    ) : (
+                                        {
+                                            fontSize: '1.3em',
+                                            color: '#fff5',
+                                        }
+                                    )
+                                }
+                            />
+                        </IconButton>
+
+                        <IconButton
+                            disabled={currentPage == 'profile'}
+
+                            onClick={() => {
+                                navigate('/profile');
+                            }}
+                            size="small"
+                        >
+                            <PersonIcon
+                                sx={
+                                    (currentPage == 'profile') ? (
+                                        {
+                                            fontSize: '1.3em',
+                                            color: '#F34213',
+                                        }
+                                    ) : (
+                                        {
+                                            fontSize: '1.3em',
+                                            color: '#fff5',
+                                        }
+                                    )
+                                }
+                            />
+                        </IconButton>
+                    </Box>
+                    <Typography
+                        sx={{
+                            color: 'transparent',
+                            lineHeight: '0'
                         }}
-                        size="small"
-
-                    >
-                        <ShoppingCartIcon
-                            sx={
-                                (currentPage == 'cart') ? (
-                                    {
-                                        fontSize: '1.3em',
-                                        color: '#F34213',
-                                    }
-                                ) : (
-                                    {
-                                        fontSize: '1.3em',
-                                        color: '#fff5',
-                                    }
-                                )
-                            }
-                        />
-                    </IconButton>
-
-                    <IconButton
-                        disabled={currentPage == 'profile'}
-
-                        onClick={() => {
-                            navigate('/profile');
-                        }}
-                        size="small"
-                    >
-                        <PersonIcon
-                            sx={
-                                (currentPage == 'profile') ? (
-                                    {
-                                        fontSize: '1.3em',
-                                        color: '#F34213',
-                                    }
-                                ) : (
-                                    {
-                                        fontSize: '1.3em',
-                                        color: '#fff5',
-                                    }
-                                )
-                            }
-                        />
-                    </IconButton>
+                    >a</Typography>
                 </Box>
 
             ) : (
